85c1c12998cd02c1e7b2de6af0d29b09ea55f44c
misakiyonehara <shiori.yonehara@gmail.com>
2016-05-15 22:42:29 +0000

    rework

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,15 +2,15 @@ class Item < ActiveRecord::Base
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
   
-  def item_title  
-    item['itemName']
-  end
+#  def item_title  
+#    item['itemName']
+#  end
   
-  def item_large_image
-    item['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
-  end
+#  def item_large_image
+#    item['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
+#  end
   
-  def item_detail_page_url
-    item['itemUrl']
-  end
+#  def item_detail_page_url
+#    item['itemUrl']
+#  end
 end
